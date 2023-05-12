f33bc4df8ca87d7e49b65b4f20bbe7aa87514338
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-05-12 22:14:47 +0530

    Add files via upload

---
+++ components/Game.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent("game-play", {
+  schema: {
+    elementId: { type: "string", default: "#ring1" },    
+  },
+  update: function () {
+    this.isCollided(this.data.elementId);
+  },
+
+  
+  isCollided: function (elemntId) {
+    const element = document.querySelector(elemntId);
+    element.addEventListener("collide", (e) => {
+      if (elemntId.includes("#ring")) {
+        
+      } else {
+        
+      }
+    });
+  },
+  
+});
